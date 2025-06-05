ebe5fc8402207bec6c4c9dceac52d5e0b2e0c420
Shaama Manoharan <110484017+Shaamam@users.noreply.github.com>
2025-06-05 17:17:12 +0530

    Update project structure for Webpack integration

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Dashboard from './pages/Dashboard';
+
+const App = () => {
+  return (
+      <div>
+        <h1>Admin Dashboard</h1>
+        <Dashboard />
+      </div>
+  );
+};
+
+export default App;
+
